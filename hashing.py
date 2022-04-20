bc27286da36892e35b495281ef8a2b576e5eb046
angelayalafinance <96507984+angelayalafinance@users.noreply.github.com>
2022-04-20 11:22:31 -0400

    Update hashing.py

--- hashing.py
+++ hashing.py
@@ -1,4 +1,5 @@
 from hashlib import sha256
+import time
 
 MAX_NONCE = 1000000000000
 
@@ -7,27 +8,30 @@ def SHA256(text):
     return sha256(text.encode("ascii")).hexdigest()
 
 
-def mine(transactions, prefix_zeros):
-    prefix_str = '0' * prefix_zeros
+def mine(input, prefix_zeros):
+    
+    prefix_str = '0' * prefix_zeros #Prefix string is x amount of zeros
+    
     for nonce in range(MAX_NONCE):
-        text = transactions + str(nonce)
+        text = input + str(nonce)    #Hash is string input + nonce
         new_hash = SHA256(text)
-        if new_hash.startswith(prefix_str):
-            print(f"Yay! Successfully mined bitcoins with nonce value:{nonce}")
-            print(nonce)
+        if new_hash.startswith(prefix_str): #If prefix string is x amount of zeros
+            print(f"Yay! Successfully mined bitcoins with nonce value: {input}{nonce}")
             return new_hash
 
     raise BaseException(f"Couldn't find correct has after trying {MAX_NONCE} times")
     
     
     
+    
+    
 if __name__=='__main__':
-    transactions='Angel Humberto '
-    difficulty=6 # try changing this to higher number and you will see it will take more time for mining as difficulty increases
-    import time
+    input ='Angel Humberto '
+    zeros=7 # try changing this to higher number and you will see it will take more time for mining as difficulty increases
     start = time.time()
-    print("start mining")
-    new_hash = mine(transactions, difficulty) #previous hash of already mined bitcoin
-    total_time = str((time.time() - start))
-    print(f"end mining. Mining took: {total_time} seconds")
+    print("Start Mining!")
+    new_hash = mine(input, zeros)
+    
+    total_time = str((time.time() - start)) #   keep track of time
+    print(f"Mining took: {total_time} seconds")
     print(new_hash)
